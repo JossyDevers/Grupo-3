1ba4a0ab472bec42abf48bbd2fab02bda0f894f6
Jossy Devers, BHDLeon <jossy_devers@bhdleon.com.do>
2020-12-09 15:08:15 -0400

    Prueba

--- Controllers/AreasController.cs
+++ Controllers/AreasController.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using StudentService.Models;
 
+// Prueba
+
 namespace StudentService.Controllers
 {
     public class AreasController : Controller
